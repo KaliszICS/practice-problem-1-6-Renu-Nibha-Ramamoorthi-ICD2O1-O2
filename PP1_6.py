098ed5f95051c7b1a2c5d0b2cf68d6525853eef7
Renu-Nibha-Ramamoorthi-ICD2O1-O2 <renunibha.ramamoorthi@student.tdsb.on.ca>
2024-11-11 14:46:13 -0500

    Update PP1_6.py

--- PP1_6.py
+++ PP1_6.py
@@ -7,32 +7,32 @@
 
 # From input, recieve two integers from the user and add them together.  Output the result.
 def q1():
-  num1 = input("Input a number: ")
-  num2 = input("Input a number: ")
-  
-  Print(num + num2)
+  num1 = int(input("Enter the first integer: ")) 
+  num2 = int(input("Enter the second integer: ")) 
+  result = num1 + num2
+  print("The sum is:", result) 	
 
 # From input recieve two integers.  Output the quotient rounded down.
 
 def q2():
-  num1 = input("Input a number: ")
-  num2 = input("Input a number: ")
-  
-  print(num1/num2)
+  num1 = int(input("Enter the first integer (divided): "))
+  num2 = int(input("Enter the second integer (divided): "))
+  quotient = num // num2 	
+  print("The quotient rounded down is:", quotien)
 
 # Output the phrase "hello Mr. Kalisz have you seen my work yet?"
 
 def q3():
-  print (hello Mr. Kalisz have you seen my work yet?)
+  print ("hello Mr. Kalisz have you seen my work yet?")
 
 # From input recieve two numbers (can be decimal fractions).  
 # Output their result multiplied together.  Then round down to the nearest whole number
 
 def q4():
-  input("Input a number: ")
-  num2 = input("Input a number: ")
-  
-  print (int(num1 * num2))
+  num1 = float(input("Enter the first number: "))
+  num2 = float(input("Enter the second number: ")) 
+  result = num1 * num2
+  print("The product rounded down is:", rounded_result)	
 
 q1()
 q2()
